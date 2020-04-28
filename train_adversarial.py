f1fb08a58f759d5a834ed715a6a638c5c00f7d1e
Abhimanyu Dubey <abhimanyu1401@gmail.com>
2020-04-27 19:52:58 -0700

    added resnet versions

--- train_adversarial.py
+++ train_adversarial.py
@@ -50,7 +50,8 @@ def train(epoch):
         main_loss.sum().backward()
 
         adv_loss = loss.data.clone() * weights
-        adv_loss = -(adv_loss.sum() - r_loss.data.clone()
+        adv_loss = -10.0*(
+            adv_loss.sum() - r_loss.data.clone()
             )/(r_loss.data.clone())
         adv_loss.backward()
 
